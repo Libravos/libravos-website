177db4fe3e69e68bd55791bab61a85657012f83a
Cole Killian <g.cole.killian@gmail.com>
2019-09-19 11:27:34 -0400

    replacing alt SharedSchool with Libravos

--- helper/navbar.php
+++ helper/navbar.php
@@ -1,6 +1,6 @@
 <nav id="navbar-top" class="navbar sticky-top navbar-expand-lg navbar-dark blue" >
 	<a class="navbar-brand" href="/">
-		<img src="/img/logo_text.png" height="40" class="d-inline-block align-top" alt="SharedSchool">
+		<img src="/img/logo_text.png" height="40" class="d-inline-block align-top" alt="Libravos">
 	</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
